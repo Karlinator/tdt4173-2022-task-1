320c5d3eb93109df63800ceea20748e048465b13
Karl Erik Hofseth <karl.e.hofseth@infomedia.dk>
2022-09-12 15:12:24 +0200

    Finish k_means implementation

--- k_means/k_means.py
+++ k_means/k_means.py
@@ -12,10 +12,11 @@ class KMeans:
     k: int
     centroids: npt.ArrayLike
     
-    def __init__(self, iterations=1):
+    def __init__(self, iterations=10, k=2):
         # NOTE: Feel free add any hyperparameters 
         # (with defaults) as you see fit
         self.iterations = iterations
+        self.k = k
         
     def fit(self, X: npt.NDArray) -> None:
         """
@@ -25,17 +26,30 @@ class KMeans:
             X (array<m,n>): a matrix of floats with
                 m rows (#samples) and n columns (#features)
         """
-        self.k = X.shape[1]
+        X = np.array(X) # Enure we get actual ndarray, and not some other stupid type.
         self.centroids = np.random.random_sample(size=(self.k, 2))
-        for i in range(self.iterations):
+        for iteration in range(self.iterations):
             # Put samples in groups
             distances = cross_euclidean_distance(X, self.centroids)
-            groups = []
+            groups = [[]] * self.k
             for j in range(self.k):
-                groups[j] = [X[x] for x in distances if min(x) == x[j]]
+                groups[j] = np.array([X[i] for i, x in enumerate(distances) if min(x) == x[j]])
+
+            for i, group in enumerate(groups):
+                if len(group) == 0:
+                    # A group is empty
+                    # This can cause problems, so we re-initialize that centroid.
+                    # If the empty group persists until the end it will cause a crash elsewhere,
+                    # and en ampty group is not being useful (we know how many groups there is supposed to be, after all)
+                    print(f"Group {i} is empty at iteration {iteration}, re-initializing")
+                    self.centroids[i] = np.random.random_sample(size=(1, 2))
+
+            if iteration % 10 == 0:
+                print (f"Finished iteration {iteration}")
 
             # Update centroids
-            self.centroids = np.array([[x / len(group) for x in np.sum(group, axis=1)] for group in groups])
+            self.centroids = np.array([[x / len(group) for x in np.sum(group, axis=0)] if len(group) > 0 else self.centroids[i] for i, group in enumerate(groups)])
+
 
     
     def predict(self, X: npt.ArrayLike) -> npt.ArrayLike:
@@ -54,7 +68,7 @@ class KMeans:
             there are 3 clusters, then a possible assignment
             could be: array([2, 0, 0, 1, 2, 1, 1, 0, 2, 2])
         """
-        distances = cross_euclidean_distance(X, self.centroids)
+        distances = cross_euclidean_distance(np.array(X), self.centroids)
         return np.array([min(range(len(distance)), key=distance.__getitem__) for  distance in distances])
     
     def get_centroids(self):
@@ -73,8 +87,10 @@ class KMeans:
         ])
         """
         return self.centroids
+
     
-    
+def normalize(X: pd.DataFrame) -> pd.DataFrame:
+    return (X - np.min(X, axis=0)) / (np.max(X, axis=0) - np.min(X, axis=0))
     
     
 # --- Some utility functions 
@@ -137,7 +153,6 @@ def cross_euclidean_distance(x: np.ndarray[float, float], y: np.ndarray[float, f
     y = x if y is None else y 
     assert len(x.shape) >= 2
     assert len(y.shape) >= 2
-    print(x, y)
     return euclidean_distance(x[..., :, None, :], y[..., None, :, :])
 
 
@@ -179,3 +194,18 @@ def euclidean_silhouette(X, z):
     b = (D + inf_mask).min(axis=1)
     
     return np.mean((b - a) / np.maximum(a, b))
+
+
+def main():
+    data_2 = pd.read_csv('k_means/data_2.csv')[:10]
+    print(normalize(data_2))
+    # data_2.describe().T
+    # X = data_2[['x0', 'x1']][:10]
+    # model_2 = KMeans(k=10, iterations=10)  # <-- Feel free to add hyperparameters 
+    # model_2.fit(X)
+    # print(model_2.predict(X))
+    # print(X)
+    # print(model_2.get_centroids())
+
+if __name__ == '__main__':
+    main()
